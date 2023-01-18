c1eec98d7211b43a3536ae36201e649d0899e89a
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2023-01-18 10:30:28 +0700

    Create responseBuilder.js

---
+++ src/helpers/responseBuilder.js
@@ -0,0 +1,14 @@
+export const successResponseBuilder = (data) => {
+    return {
+        success: true,
+        data: data,
+    };
+};
+
+export const errorResponseBuilder = (error) => {
+    return {
+        success: false,
+        error: error.message,
+    };
+};
+
