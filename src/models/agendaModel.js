cf790dfb5768d022dbf436e9ea93ba34d9efb838
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2023-01-18 09:42:30 +0700

    Create agendaModel.js

---
+++ src/models/agendaModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const agendaSchema = new mongoose.Schema(
+    {
+        tanggal: {
+            type: Date,
+            required: true,
+        },
+        waktu: {
+            type: String,
+            required: true,
+        },
+        host: {
+            type: String,
+            required: true,
+        },
+        peserta: {
+            type: [String],
+            required: true,
+        },
+        topik: {
+            type: String,
+            required: true,
+        },
+        tempat: {
+            type: String,
+            required: true,
+        },
+        keterangan: {
+            type: String,
+            required: true,
+        },
+        suratPinjam: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    {timestamps: true}
+);
+
+const Agenda = mongoose.model('Agenda', agendaSchema);
+
+export default Agenda;
