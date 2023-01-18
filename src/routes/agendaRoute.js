6680d9b83660d13f3a7138feb2ebe1cdd6321d38
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2023-01-18 11:55:37 +0700

    Update agendaRoute.js

--- src/routes/agendaRoute.js
+++ src/routes/agendaRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as controller from '../controllers/agendaController.js';
-import * as auth from '../middlewares/auth.js';
+// import * as auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
