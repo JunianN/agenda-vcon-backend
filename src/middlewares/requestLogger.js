aa2d22bff6f2c1fe08360055b17d2f582466a6fe
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2023-01-20 09:36:49 +0700

    Create requestLogger.js

---
+++ src/middlewares/requestLogger.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+
+const requestLogger = (req,res, next) => {
+    const current_date_time = new Date();
+    const formatted_date = 
+        String(current_date_time.getFullYear()).padStart(2, '0');
+    const method = String(req.method).padEnd(8);
+    const url = req.url;
+
+    const log = `[${formatted_date}] ${method} ${url}`;
+
+    console.log(log);
+
+    fs.appendFile('logs/requestLogs.txt', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    next();
+};
+
+export default requestLogger;
