55752209304a369bedae89ec16058fd12be938d0
Rizky Junian Nugroho <rizky.j.n@mail.ugm.ac.id>
2023-01-27 08:01:56 +0700

    Update agendaRoute.js

--- src/routes/agendaRoute.js
+++ src/routes/agendaRoute.js
@@ -1,22 +1,22 @@
 import express from 'express';
 import * as controller from '../controllers/agendaController.js';
-// import * as auth from '../middlewares/auth.js';
+import * as auth from '../middlewares/auth.js';
 
 const router = express.Router();
-
-//Membuat agenda baru.
-router.post('/', controller.create);
 
 //Melihat semua agenda
 router.get('/', controller.findAll);
 
-//Menghapus agenda dengan id tertentu
-router.delete('/:id', controller.deleteById);
-
-//mengubah agenda dengan id tertentu
-router.put('/:id', controller.updateById);
-
 //melihat agenda dengan id tertentu
 router.get('/:id', controller.findById);
+
+//Membuat agenda baru.
+router.post('/', auth.authenticate, controller.create);
+
+//Menghapus agenda dengan id tertentu
+router.delete('/:id', auth.authenticate, controller.deleteById);
+
+//mengubah agenda dengan id tertentu
+router.put('/:id', auth.authenticate, controller.updateById);
 
 export default router;
